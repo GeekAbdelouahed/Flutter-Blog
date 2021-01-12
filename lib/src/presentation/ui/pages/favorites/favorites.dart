5dfb6492404b40c1767521167c1ef46c54e43ee4
Abdelouahed <medjoudja@sindibadinternational.net>
2021-01-12 20:57:42 +0100

    auth on favorite

--- lib/src/presentation/ui/pages/favorites/favorites.dart
+++ lib/src/presentation/ui/pages/favorites/favorites.dart
@@ -6,6 +6,7 @@ import '../../../../infrastructure/core/preferences.dart';
 import '../../../../injection.dart';
 import '../../components/article_item.dart';
 import '../../components/loading.dart';
+import '../../components/login.dart';
 
 class FavoritesPage extends StatefulWidget {
   @override
@@ -34,27 +35,29 @@ class _FavoritesPageState extends State<FavoritesPage> {
 
   @override
   Widget build(BuildContext context) => SafeArea(
-        child: RefreshIndicator(
-          onRefresh: _loadData,
-          child: BlocBuilder<FavoritesBloc, FavoritesState>(
-            cubit: _bloc,
-            builder: (_, state) => state.favoritesState.fold(
-              () => LoadingWidget(),
-              (either) => either.fold(
-                (apiError) => Text(apiError.message ?? 'Unknown error!'),
-                (result) => ListView.builder(
-                  padding: const EdgeInsets.only(
-                    top: 20,
-                    bottom: 100,
-                  ),
-                  itemCount: result.data.length,
-                  itemBuilder: (_, index) => ArticleItemWidget(
-                    article: result.data[index].article,
+        child: _preferences.isLoggedIn
+            ? RefreshIndicator(
+                onRefresh: _loadData,
+                child: BlocBuilder<FavoritesBloc, FavoritesState>(
+                  cubit: _bloc,
+                  builder: (_, state) => state.favoritesState.fold(
+                    () => LoadingWidget(),
+                    (either) => either.fold(
+                      (apiError) => Text(apiError.message ?? 'Unknown error!'),
+                      (result) => ListView.builder(
+                        padding: const EdgeInsets.only(
+                          top: 20,
+                          bottom: 100,
+                        ),
+                        itemCount: result.data.length,
+                        itemBuilder: (_, index) => ArticleItemWidget(
+                          article: result.data[index].article,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
-        ),
+              )
+            : AskLoginWidget(),
       );
 }
